088ddf4d21d9cbbc23bba396e612742735c2e32e
GITlisalisa <chao_lisa_jinhee@student.smc.edu>
2021-11-17 23:16:19 -0800

    comment out ball size change

--- week_11/hw-redo/canvas.js
+++ week_11/hw-redo/canvas.js
@@ -2,19 +2,15 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext('2d');
 
-// canvas size, ball velocity, ball size
 let cPosX = canvas.width/2;
 let cPosY = canvas.height/2;
 let cVelX = 2;
 let cVelY = 1;
 let cRadius = 60;
-let newRadiusValue = 90;
+// let newRadiusValue = 90;
 let ballColor = "hsl(180, 50%, 50%)";
-
 var audio = new Audio('sound.mp3');
 
-
-// Function to draw canvas 
 function draw(){
     context.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -23,7 +19,7 @@ function draw(){
 
     window.requestAnimationFrame(draw);
 }
-// Function to draw ball 
+
 function drawCircle(){
     context.beginPath();
     context.strokeStyle = "black";
@@ -48,12 +44,9 @@ function moveCircle(){
     cPosX = cPosX + cVelX;
     cPosY = cPosY + cVelY;
 }
-
 draw();
 
 
-
-// funtion to change ball color
 const hueSlider = document.getElementById("hueValue");
 const saturationSlider = document.getElementById("satValue");
 const lightnessSlider = document.getElementById("lightValue");
@@ -73,45 +66,38 @@ lightnessSlider.addEventListener("change", calculateColor);
 
 
 // funtion to change ball size 
-const sizeSlider = document.getElementById("ballSize");
+// const sizeSlider = document.getElementById("ballSize");
 
-let calculateSize = function(){
-    let valueR = sizeSlider.value;
-    let newRadiusValue = valueR;
-    cRadius = newRadiusValue;
-    console.log("Size change!!!");
-}
+// let calculateSize = function(){
+//     let valueR = sizeSlider.value;
+//     let newRadiusValue = valueR;
+//     cRadius = newRadiusValue;
+//     console.log("Size change!!!");
+// }
 
-sizeSlider.addEventListener("change", calculateSize);
-function canvasPressed() {
-    // playing a sound file on a user gesture
-    // is equivalent to `userStartAudio()`
-    mySound.play();
-  }
+// sizeSlider.addEventListener("change", calculateSize);
+
 
 // Function to count number times ball is clicked
 var counterVal = 0;
 canvas.addEventListener("click", function(event){
-   //console.log(event);
   
    let mouseXp = event.pageX - event.target.offsetLeft;
    let mouseYp = event.pageY - event.target.offsetTop;
-   console.log("Mouse X: " + mouseXp + " Mouse Y: " + mouseYp);
+//    console.log("Mouse X: " + mouseXp + " Mouse Y: " + mouseYp);
 
    let distX = Math.abs(cPosX - mouseXp);
    let distY = Math.abs(cPosY - mouseYp);
  
    if(distX < cRadius && distY < cRadius){
-    console.log("HIT!!!");
+    console.log("HIT");
     ++counterVal;
     document.getElementById("clickCount").innerHTML =  "Click Counter: " + counterVal;
-    cVelX = cVelX * 1.25;
-    cVelY = cVelY * 1.25;
-    // document.getElementById("game").style.backgroundColor = "black";
+    cVelX = cVelX * 1.5;
+    cVelY = cVelY * 1.5;
+    audio.play();
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById("game").style.backgroundColor = "#" + randomColor;
-    // color.innerHTML = "#" + randomColor;
-    audio.play();
 }
 });
 
