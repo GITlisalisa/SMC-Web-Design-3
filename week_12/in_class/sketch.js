bd6a6773ee14ae6eb36cabd04b3a65b24ada7a71
GITlisalisa <chao_lisa_jinhee@student.smc.edu>
2021-11-20 14:07:31 -0800

    in_class exercise week12 ball game

--- week_12/in_class/sketch.js
+++ week_12/in_class/sketch.js
@@ -7,20 +7,28 @@ let velY;
 let radius;
 let circleClr;
 
+//UI
+let sliderHue;
+let clickCounter;
+
 function setup(){ // setup runs once, sets up the canvas
-    let myCanvas = createCanvas(800,600);
+    let myCanvas = createCanvas(800, 600);
     myCanvas.parent('canvasParent');
 
     colorMode(HSB, 360, 100, 100);
-
+    sliderHue = createSlider(0, 360, 0, 15);
+    sliderHue.parent('canvasUI');
+    sliderHue.style('width', '100px');
     posX = width/2;
     posY = height/2;
    
-    velX = 4;
-    velY = 3;
+    velX = 2;
+    velY = 1;
 
-    let radius = 50;
+    radius = 50;
     circleClr = color(0, 80, 100);
+
+    clickCounter = 0;
 }
 
 function draw(){ // draw runs on a loop over and over in the canvas
@@ -30,11 +38,18 @@ background (120, 40 ,100);
 noFill();
 stroke(270, 80, 80);
 strokeWeight(10);
-rect(200, 150, 200, 300);
+rect(5, 5, 70, 30);
 
-fill( circleClr);
+textSize(24);
+fill(270, 80, 80);
+strokeWeight(2);
+text(clickCounter, 12, 28);
+
+
 noStroke();
-circle(posX,posY,radius * 2);
+circleClr = color(sliderHue.value(), 80, 100);
+fill( circleClr);
+circle(posX, posY, radius * 2);
 if (posX + radius >= width || posX - radius <=0){
     velX = velX * -1;
 }
@@ -48,10 +63,23 @@ posY += velY;
 
 function mouseReleased() {
     if (dist(mouseX, mouseY, posX, posY) < radius){
-        circleClr = color(255, 0, 0);
-    // radius +=10;
+        clickCounter++;
+        // circleClr = color(255, 0, 0);
+        // radius +=10;
+        if(velX >0 ){
+            velX++;
+        }
+        else {
+            velX--;
+        }
+        if(velY >0 ){
+            velY++;
+        }
+        else {
+            velY--;
+        }
     }
     else{
-        circleClr = color(204,102,0);
+        // circleClr = color(0, 80, 100);
     }
 }
