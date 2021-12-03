19b2d49c02afd3cf4b09f0d425be248e375e1eec
GITlisalisa <chao_lisa_jinhee@student.smc.edu>
2021-12-02 23:42:21 -0800

    create blank animation file, change colors, positioning

--- final-design/sketch.js
+++ final-design/sketch.js
@@ -1,18 +1,23 @@
 
+var audio = new Audio('golf-swing.wav');
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll('.reveal');
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
+        
       } else {
         reveals[i].classList.remove("active");
       }
     }
+    audio.play();
   }
+window.addEventListener("scroll", reveal);
 
-  window.addEventListener("scroll", reveal);
+
+
 
